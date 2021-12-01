1741d4125706c40c2c7a4a09a8d95ed54ff88496
ahmedsharyo <ahmedsharyo@gmail.com>
2021-12-01 13:13:00 +0200

    security refactor

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 
 	Config "github.com/ahmedsharyo/AccessControlService/config"
 	Models "github.com/ahmedsharyo/AccessControlService/models"
-	"github.com/gin-gonic/gin"
 
 	//"github.com/ahmedsharyo/AlertService/Routes"
 
@@ -27,12 +26,8 @@ func main() {
 	defer Config.DB.Close()
 
 	Config.DB.AutoMigrate(&Models.SecurityMangerCameras{})
-	server := gin.New()
 
 	//Routes.Setup(app)
 	go grpc_service.Run()
-
-	port := "8081"
-	server.Run(":" + port)
 
 }
